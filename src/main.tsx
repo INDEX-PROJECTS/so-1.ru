fcae455bd72d80b36c4ded52c6eef6c6d52a51d2
nayvok <44442827+nayvok@users.noreply.github.com>
2025-01-24 14:52:12 +0500

    feat: remove default favicon and set favicon dynamically based on environment variable

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,26 @@ if (!container) {
     throw new Error('Контейнер root не найден. Не удалось вмонтировать реакт приложение');
 }
 
+function setFavicon() {
+    let favicon = '/favicon.svg';
+
+    if (import.meta.env.VITE_DOMEN_NAME === 'pro-zapchasti.online') {
+        favicon = '/favicon2.svg';
+    }
+
+    const link: HTMLLinkElement = document.querySelector("link[rel='icon']") || document.createElement('link');
+    link.rel = 'icon';
+    link.type = 'image/svg+xml';
+    link.href = favicon;
+
+    if (!link.parentNode) {
+        document.head.appendChild(link);
+    }
+}
+
+// Вызов функции смены фавикона
+setFavicon();
+
 const root = createRoot(container);
 
 root.render(

94b59e0b663c92736621ad9bc19ddd14418db7a1
nayvok <44442827+nayvok@users.noreply.github.com>
2025-01-30 17:28:32 +0500

    feat: implement payment integration and update button logic based on environment settings.

--- src/features/Cart/ui/LeaveOrderForm/LeaveOrderForm.tsx
+++ src/features/Cart/ui/LeaveOrderForm/LeaveOrderForm.tsx
@@ -1,4 +1,6 @@
 import { memo, useState } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import styles from './LeaveOrderForm.module.scss';
 import { Text, TextBold, TextSize } from '@/shared/ui/Text/Text';
@@ -9,6 +11,8 @@ import { Input } from '@/shared/ui/Input/Input';
 import { ReactComponent as CloseIcon } from '@/shared/assets/icons/close-icon.svg';
 import { useAppDispatch } from '@/app/Redux/store';
 import { addNotification } from '@/app/Redux/notifications/slice';
+import { formatNumber } from '@/shared/utils/formatNumber.ts';
+import { selectCart } from '@/app/Redux/cart/selectors.ts';
 
 interface LeaveOrderFormProps {
   className?: string;
@@ -26,6 +30,8 @@ export const LeaveOrderForm = memo(({
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+
+    const { totalPrice } = useSelector(selectCart);
 
     const onChangeName = (value: string) => {
         setName(value);
@@ -56,6 +62,31 @@ export const LeaveOrderForm = memo(({
         }
     };
 
+    const paymentYooKassa = async () => {
+        await axios
+            .post(import.meta.env.VITE_PAYMENT_LINK, {
+                amount: totalPrice,
+                email,
+            })
+            .then((response) => {
+                onSuccess();
+                dispatch(addNotification({
+                    text: 'Переадресация...',
+                }));
+                setName('');
+                setEmail('');
+                if (onClearCart) {
+                    onClearCart();
+                }
+                window.location.replace(response.data.paymentUrl);
+            })
+            .catch(() => {
+                dispatch(addNotification({
+                    text: 'Что-то пошло не так! Повторите попытку позже.',
+                }));
+            });
+    };
+
     return (
         <VStack max align="start" justify="between" className={classNames(styles.LeaveOrderForm, {}, [className])}>
             <HStack max justify={isBack ? 'between' : 'end'}>
@@ -78,7 +109,7 @@ export const LeaveOrderForm = memo(({
             <VStack max align="start" gap="50">
                 <VStack max gap="16" align="start">
                     <Input value={name} onChange={onChangeName} placeholder="Ф.И.О" />
-                    <Input value={email} onChange={onChangeEmail} placeholder="Email / Номер" />
+                    <Input value={email} onChange={onChangeEmail} placeholder="Email" />
                 </VStack>
 
                 <Checkbox
@@ -89,14 +120,35 @@ export const LeaveOrderForm = memo(({
                 />
             </VStack>
 
-            <Button
-                onClick={handleFormSubmit}
-                disabled={!checked || isNameInvalid || isEmailInvalid}
-                theme={ThemeButton.DEFAULT}
-                className={styles.sendBtn}
-            >
-                <Text gap="0" isActive title="Отправить" size={TextSize.S} bold={TextBold.LIGHT} />
-            </Button>
+            {
+                import.meta.env.VITE_DOMEN_NAME === 'pro-zapchasti.online'
+                    ? (
+                        <Button
+                            onClick={paymentYooKassa}
+                            disabled={!checked}
+                            theme={ThemeButton.DEFAULT_BETWEEN}
+                        >
+                            <Text gap="0" isActive title="Оплатить" size={TextSize.S} bold={TextBold.LIGHT} />
+                            <Text
+                                gap="0"
+                                isActive
+                                title={`${formatNumber(totalPrice)} ₽`}
+                                size={TextSize.M}
+                                bold={TextBold.MEDIUM}
+                            />
+                        </Button>
+                    )
+                    : (
+                        <Button
+                            onClick={handleFormSubmit}
+                            disabled={!checked || isNameInvalid || isEmailInvalid}
+                            theme={ThemeButton.DEFAULT}
+                            className={styles.sendBtn}
+                        >
+                            <Text gap="0" isActive title="Отправить" size={TextSize.S} bold={TextBold.LIGHT} />
+                        </Button>
+                    )
+            }
         </VStack>
     );
 });
